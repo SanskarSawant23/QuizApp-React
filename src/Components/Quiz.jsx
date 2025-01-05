fdb38621326d6710d2ca5362a7e7a3ff245afb9d
sanskar sawant <sanskarsawant23@gmail.com>
2025-01-05 18:21:14 +0530

    added useRef

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -1,18 +1,32 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { questions } from '../assets/questions';
 const Quiz = () => {
   let [index, setIndex] = useState(0);
   let [question, setQuestion ] = useState(questions[index]); //question variable has the questions array's element.
+  const [lock, setLock] = useState(false);
 
+  let Option1 = useRef(null);
+  let Option2 = useRef(null);
+  let Option3 = useRef(null);
+  let Option4 = useRef(null); 
+
+  let Option_array = [Option1, Option2, Option3, Option4];
+    
   const onClick = (e, ans)=>{
-     console.log(question.ans)
+   if(lock === false){
     if(question.ans === ans){   
-        console.log("correct ans");  
-        e.target.classList.add("correct");
-    }else{
-        console.log("wrong ans");
-        e.target.classList.add("wrong");
-    }
+      console.log("correct ans");  
+      e.target.classList.add("correct");
+  }else{
+      console.log("wrong ans");
+      e.target.classList.add("wrong");
+      Option_array[question.ans - 1] 
+
+  }
+    setLock(true);
+
+   }
+    
   }
   return (
     <div className="h-screen flex grid gird-cols-1 justify-center items-center bg-gray-100">
@@ -21,10 +35,10 @@ const Quiz = () => {
         
       <h2 className="mt-4">{index+1}. {question.question}</h2>
       <ul className="list-disc mt-4 text-left cursor-pointer">
-        <li onClick={(e)=>{onClick(e,1)}}>{question.option1}</li>
-        <li onClick={(e)=>{onClick(e,2)}}>{question.option2}</li>
-        <li onClick={(e)=>{onClick(e,3)}}>{question.option3}</li>
-        <li onClick={(e)=>{onClick(e,4)}}>{question.option4}</li>
+        <li ref={Option1} onClick={(e)=>{onClick(e,1)}}>{question.option1}</li>  {/* every li tag has a refrence whenever we click on the wrong option then we call the refrence of the correct option*/}
+        <li ref={Option2} onClick={(e)=>{onClick(e,2)}}>{question.option2}</li>
+        <li ref={Option3} onClick={(e)=>{onClick(e,3)}}>{question.option3}</li>
+        <li ref={Option4} onClick={(e)=>{onClick(e,4)}}>{question.option4}</li>
       </ul>
       <button className="mt-6 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">Next</button>
       <div className="mt-4 text-gray-500">1 of 5 questions</div>
